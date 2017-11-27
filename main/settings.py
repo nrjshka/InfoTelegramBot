bc6be19677e4540e81689c21a971be182f2308ef
nrjshka <nrjshka@gmail.com>
2017-11-27 22:26:24 +0300

    Decorationg commit

--- main/settings.py
+++ main/settings.py
@@ -110,4 +110,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/django/site/static'
+STATIC_ROOT = '/static/'
+#STATIC_ROOT = '/home/django/site/static'
